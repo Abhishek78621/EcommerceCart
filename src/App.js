96439f17772220389682098d8ef9973ce2161a6f
Abhishek <156158161+Abhishek78621@users.noreply.github.com>
2024-01-31 15:27:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 //import Products from "./Components/Products";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-import {BrowserRouter as  Router,Routes,Route} from "react-router-dom";
+import {BrowserRouter as  BrowserRouter,Routes,Route} from "react-router-dom";
 import Checkout from "./Components/Checkout";
 import Homeproducts from "./Components/Homeproducts";
 
@@ -10,16 +10,16 @@ function App() {
   return (
     <div className="App">
 
-    <Router>
+    <BrowserRouter baseName="/EcommerceCart">
       
         <Routes>
-          <Route path='/' element={<><Header/><Home/></>}></Route>
+          <Route path='/EcommerceCart' element={<><Header/><Home/></>}></Route>
           <Route path='/home' element={<><Header/><Home/></>}></Route>
           <Route path="/homeproducts" element={<><Header/><Homeproducts/></>}></Route>
           <Route path='/checkout' element={<><Header/><Checkout/></>}></Route>
           </Routes>
        
-      </Router>
+      </BrowserRouter>
  
     </div>
   );
